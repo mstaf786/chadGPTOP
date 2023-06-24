1846c55f8cfca94bcbe71759d9298097eca23ae6
mstaf786 <mpatrawala123@gmail.com>
2023-06-24 16:54:51 +0530

    add bundle.js and tried lodash

--- emailer.js
+++ emailer.js
@@ -1,3 +1,7 @@
-import { without } from "lodash";
+var _ = require("lodash");
 
-console.log(without);
+console.log(_);
+
+var array = [1, 2, 3, 4, 5, 6];
+
+console.log("answer:", _.without(array, 3));
